519f9d59b845cb9613a762c28afc901dcee6afd1
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-09 17:15:21 +0100

    [WiP] temporarily console.log all environment variables on Lambda

--- index.js
+++ index.js
@@ -6,32 +6,37 @@ const AWS = require('aws-sdk');
 const myCredentials = new AWS.EnvironmentCredentials('AWS'); // Lambda provided credentials
 console.log(myCredentials);
 // return;
-
-var lambdas_to_trigger = [ // this *could* be an environment variable ...
-  // 'lambda-taggable-cloudsearch-indexer-v1',
-  'lambda-taggable-elasticsearch-indexer-v1',
-  'lambda-taggable-neo4j-indexer-v1'
-];
-
-exports.handler = function (event, context, callback) {
-  if (!event.Records) { // Check if an tag id is provided
-    return callback(new Error('invalid event', JSON.stringify(event)));
-  }
-  // console.log(JSON.stringify(event.Records[0].s3, null, 2));
-  var countdown = lambdas_to_trigger.length * event.Records.length;
-  var res = []; // array of all responses from invoking the indexers
-  event.Records.forEach(function (record) {
-    console.log('S3 Record Key:', JSON.stringify(record.s3.object.key, null, 2));
-    var single_record_event = {'Records': [record]};
-    lambdas_to_trigger.forEach(function (name) {
-      invoke_lambda(name, single_record_event, function (err, data) {
-        AwsHelper.failOnError(err, event, context);
-        delete data.LogResult;
-        res.push(data); // accumlate all results
-        if (--countdown === 0) {
-          return callback(err, res);
-        }
-      });
-    });
-  });
+exports.handler = function(event, context) {
+  console.log(process.env);
+  console.log(myCredentials);
+  context.succeed(myCredentials);
 };
+
+// var lambdas_to_trigger = [ // this *could* be an environment variable ...
+//   // 'lambda-taggable-cloudsearch-indexer-v1',
+//   'lambda-taggable-elasticsearch-indexer-v1',
+//   'lambda-taggable-neo4j-indexer-v1'
+// ];
+//
+// exports.handler = function (event, context, callback) {
+//   if (!event.Records) { // Check if an tag id is provided
+//     return callback(new Error('invalid event', JSON.stringify(event)));
+//   }
+//   // console.log(JSON.stringify(event.Records[0].s3, null, 2));
+//   var countdown = lambdas_to_trigger.length * event.Records.length;
+//   var res = []; // array of all responses from invoking the indexers
+//   event.Records.forEach(function (record) {
+//     console.log('S3 Record Key:', JSON.stringify(record.s3.object.key, null, 2));
+//     var single_record_event = {'Records': [record]};
+//     lambdas_to_trigger.forEach(function (name) {
+//       invoke_lambda(name, single_record_event, function (err, data) {
+//         AwsHelper.failOnError(err, event, context);
+//         delete data.LogResult;
+//         res.push(data); // accumlate all results
+//         if (--countdown === 0) {
+//           return callback(err, res);
+//         }
+//       });
+//     });
+//   });
+// };
